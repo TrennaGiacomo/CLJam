ca8e6d244dd0382371b8c31f4027095430a4fadc
Giacomo Trenna <ae2342@student.jamk.fi>
2025-05-31 10:29:05 +0300

    Added Laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float rayLength = 4.5f;
+    [SerializeField] private Transform rayOrigin;
+    [SerializeField] private LayerMask targetLayer; //Player layer
+    [SerializeField] private Vector2 facingDir;
+
+    private bool isEnabled = true;
+
+    private void Update()
+    {
+        if (!isEnabled) return;
+
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin.position, facingDir, rayLength, targetLayer);
+
+        if (hit.collider != null && hit.collider.TryGetComponent(out PlayerInteraction playerInteraction))
+        {
+            Debug.Log("Player Spotted");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (rayOrigin == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(rayOrigin.position, rayOrigin.position + (Vector3)(facingDir.normalized * rayLength));
+    }
+}
